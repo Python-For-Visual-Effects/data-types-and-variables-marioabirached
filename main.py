93c45cf8195adea073994dd14d67aff64dcfaafe
marioabirached <mario-abirached@hotmail.com>
2020-10-03 20:29:23 +0300

    All Answers

--- main.py
+++ main.py
@@ -10,13 +10,26 @@ print(64+32)
 
 # 2.- Do the same as the question one but this time use variables instead of 
 # numbers.
+number_1 = 64
+number_2 = 32
+print(number_1 + number_2)
 
 # 3.- Make a program that prints a sentence that includes at least 3 variables.
+full_name = "Mario Abi Rached"
+age = 23
+studies = "Visual Effects"
+
+print("Hello, I'm " + full_name + ", I'm currently " + str(age) + " years old and I decided to study " + studies + " in 2020.")
 
 # 4.- Given a sentence, assign the string to a variable then print the number of 
 # characters in the sentence. 
 # The sentence is "This is my first Python program."
+sentence = "This is my first Python program"
+print(len(sentence))
 
 # 5.- Given the resolution 1920 x 1080, make a program that prints a string with 
 # the 10% over-scan value of those numbers. The printed string must be as 
 # follows: "The 10% overscan of 1920 is <value 1>, and the 1080 is <value 2>"
+value_1 = (1920 * 110 / 100)
+value_2 = (1080 * 110 / 100)
+print("The 10% overscan of 1920 is " + str(value_1) + ", and the 1080 is " + str(value_2) + ".")
